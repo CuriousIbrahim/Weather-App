f59a3fef88b9a36242a4b2b435bebec58764d560
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-08 01:20:23 -0400

    Hello, World! in express

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Weather API: https://openweathermap.org/current
+
+const express = require('express')
+
+var app = express();
+
+// Listen to port
+app.listen('3000')
+
+app.get('/', (req, res) => res.send('Hello, World!'))
