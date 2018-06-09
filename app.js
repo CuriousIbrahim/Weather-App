f1545ef25fe9c30db56f177fdf0db8b0fcd24bab
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-08 21:15:54 -0400

    Test index page in public/pages/index/. Use mustache as view engine, and load static folder as ./public

--- app.js
+++ app.js
@@ -3,17 +3,22 @@
 const express = require('express')
 const Weather = require('./weather.js')
 
+var app = express();
+app.set('view engine', 'mustache');
+app.use(express.static('./public'))
+
 var weather = new Weather();
 
-var app = express();
-
-let data = weather.weather('london');
-
-data.then((data) => {
-  console.log(data);
-})
+// let data = weather.weather('london');
+//
+// data.then((data) => {
+//   console.log(data);
+// })
 
 // Listen to port
 app.listen('3000')
 
-app.get('/', (req, res) => res.send('Hello, World!'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/pages/index/index.html');
+  // res.send('Hello World!');
+})
