f335f4d5f1a14bf6674eed97499bc599a5bd0535
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-09 12:58:23 -0400

    Add .get('/weather'), get info from weather api, and utitlize weather-util.js, and set engine as ejs

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-// Weather API: https://openweathermap.org/current
-
 const express = require('express')
+const path = require('path')
+const math = require('mathjs')
+
 const Weather = require('./weather.js')
+const weatherConverter = require('./weather-util.js')
 
 var app = express();
-app.set('view engine', 'mustache');
-app.use(express.static('./public'))
+
+// set view engine
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
 
 var weather = new Weather();
 
-// let data = weather.weather('london');
-//
-// data.then((data) => {
-//   console.log(data);
-// })
+// listen to port
+app.listen('3000');
 
-// Listen to port
-app.listen('3000')
-
+// index
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/pages/index/index.html');
   // res.send('Hello World!');
+});
+
+// weather
+app.get('/weather', (req, res) => {
+
+  weather.weather('london').then((data) => {
+    let temp = data.main.temp;
+    let temp_unit = 'K';
+    let description = data.weather.description;
+    let humidity = data.main.humidity;
+    let wind = data.wind.speed;
+    let windDirection = data.wind.deg + " degrees";
+
+    res.render('weather', {
+
+      temp: math.round(weatherConverter(temp, 'c')),
+      temp_unit: temp_unit,
+      description: description,
+      humidity: humidity,
+      wind: wind,
+      windDirection: windDirection
+    });
+  }).catch((err) => {
+    console.log(err);
+  })
 })
