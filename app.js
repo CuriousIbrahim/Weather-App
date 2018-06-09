2fecfcec564449b6ea094442885ae5f4ac1f2f94
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-08 20:19:24 -0400

    Test the weather.js in app,js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // Weather API: https://openweathermap.org/current
 
 const express = require('express')
+const Weather = require('./weather.js')
+
+var weather = new Weather();
 
 var app = express();
+
+let data = weather.weather('london');
+
+data.then((data) => {
+  console.log(data);
+})
 
 // Listen to port
 app.listen('3000')
