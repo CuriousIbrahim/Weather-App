6b1d4049d41adefa826974a98d9bb7178da42b89
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-09 20:17:33 -0400

    eperate into own js file. Now sends a redirect url with query strings that include long and lat, no ajax

---
+++ routes/locate.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const math = require('mathjs');
+
+const weatherConverter = require('../weather-util.js');
+const Weather = require('../weather-api.js');
+
+var weather = new Weather();
+
+var router = express.Router();
+
+var jsonParser = bodyParser.json();
+
+router.post('/', jsonParser, (req, res) => {
+
+  res.status(200).send({
+    result: 'redirect',
+    url: '/weather?lat=' + req.body.lat + '&long=' + req.body.long
+  });
+
+});
+
+// res.sendFile(__dirname + '/public/pages/index/index.html')
+
+module.exports = router;
