2547fdab5cb7ba01d00af9cc48907c0260ac0f8f
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-09 22:17:39 -0400

    Output log requested url in the weather api

--- weather-api.js
+++ weather-api.js
@@ -18,6 +18,7 @@ module.exports = class Weather {
   // returns a Promise
   async getFromCity(city) {
     let url = MAIN_URL + '?q=' + city + '&APPID=' + API_KEY;
+    console.log(url);
     return await axios.get(url).then((res) => {
       return res.data;
     })
