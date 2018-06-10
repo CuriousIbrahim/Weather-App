3c790d87c61d14cfc6c5ee78471fe96e89f3081e
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-09 20:18:06 -0400

    Add button that then starts geolocation, and redirect after done

--- public/pages/index/locate.js
+++ public/pages/index/locate.js
@@ -1,5 +1,44 @@
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position)
-  })
-} else {}
+$('#find-me').on('click', () => {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position)
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+
+      let data = {
+        lat: lat,
+        long: long
+      };
+
+      console.log(data);
+
+      $.post('/locate', data).done((res) => {
+        console.log('done');
+        window.location.replace(res.url);
+        // window.location.href = res;
+      })
+
+      // $.ajax('weather', {
+      //   type: 'POST',
+      //   data: JSON.stringify(data),
+      //   dataType: 'json',
+      //    url: '/weather',
+      //   success: (data) => {
+      //     console.log('success')
+      //   },
+      //   error: (err) => {
+      //     console.log('There seems to have been an error');
+      //   },
+      //   comlete: () => {
+      //     console.log('Complete')
+      //   }
+      // }).done(() => {
+      //   console.log('done');
+      // }).fail((err) => {
+      //   console.log(err);
+      //   console.log('fail');
+      // });
+
+    })
+  } else {}
+});
