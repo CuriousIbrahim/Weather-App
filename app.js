47703971a45d217128b5116157d35aa9b55ec0d1
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-31 17:01:36 -0400

    Store port in const, use that const in app.listen and console.log

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const helmet = require('helmet')
-const path = require('path')
-const math = require('mathjs')
-const bodyParser = require('body-parser')
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+const math = require("mathjs");
+const bodyParser = require("body-parser");
 
-const Weather = require('./weather-api.js')
-const weatherConverter = require('./weather-util.js')
+const Weather = require("./weather-api.js");
+const weatherConverter = require("./weather-util.js");
 
 var app = express();
 
@@ -13,28 +13,30 @@ var app = express();
 app.use(helmet());
 
 // use bodyParser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // set view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // use ./public as static
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 // create Weather object
 var weather = new Weather();
 
 // listen to port
-app.listen(process.env.PORT || '8000');
-console.log('Listening to port 8000');
+const port = process.env.PORT || "8000";
+
+app.listen(port);
+console.log(`Listening to port ${port}`);
 
 // get routes
-var routes = require('./routes/index.js');
-var weather = require('./routes/weather.js');
-var locate = require('./routes/locate.js')
+var routes = require("./routes/index.js");
+var weather = require("./routes/weather.js");
+var locate = require("./routes/locate.js");
 
 // set routes
-app.use('/', routes);
-app.use('/weather', weather);
-app.use('/locate', locate)
+app.use("/", routes);
+app.use("/weather", weather);
+app.use("/locate", locate);
